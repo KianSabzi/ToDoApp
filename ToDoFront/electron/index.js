8523d020a2d6c90802476c801d3871e2b0dd8647
KianSabzi <sabzi.morteza@gmail.com>
2023-01-26 13:06:43 +0330

    ProjectStructure

---
+++ ToDoFront/electron/index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindows, ipcMain, BrowserWindow} = require("electron");
+const { join } = require("path");
+
+
+const isDev = !app.isPackaged;
+app.whenReady()
+    .then(main);
+
+
+function main() {
+    const window = new BrowserWindow({
+        width: 800, height: 600, show: false,
+        autoHideMenuBar:true,
+        webPreferences: {
+            preload: join(__dirname, "./preload.js")
+        }
+    });
+
+    window.loadFile(join(__dirname,"../public/index.html"));
+    window.on("ready-to-show", window.show);
+    
+    if(isDev) window.webContents.openDevTools();
+
+}
